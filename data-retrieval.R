0abe879d71535a07436447a5f099b57533ddfc86
“Dean <deanhardy@gmail.com>
2022-02-23 16:29:07 -0500

    ...

--- data-retrieval.R
+++ data-retrieval.R
@@ -18,20 +18,20 @@ datadir <- file.path('/Users/dhardy/Dropbox/r_data/census-explorer')
 alb <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-84 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs" ## http://spatialreference.org/ref/sr-org/albers-conic-equal-area-for-florida-and-georgia/
 
 ## define census year, geography, & variables
-DAT <- 'pl'
-YR <- 2020
-ST <- c('GA')
-CNTY <- c('Chatham')
-# VAR = c(white = "B03002_003E", black = "B03002_004E",
-#         native_american = "B03002_005E", asian = "B03002_006E",
-#         hawaiian = "B03002_007E", other = "B03002_008E",
-#         multiracial = "B03002_009E", latinx = "B03002_012E", total = "B03002_001E",
-#         medhhinc = "B19049_001E", agghhinc = "B19025_001E", hu = "B25001_001E")
+DAT <- 'acs5'
+YR <- 2013
+ST <- c('SC')
+CNTY <- c('Chesterfield')
+VAR = c(white = "B03002_003E", black = "B03002_004E",
+        native_american = "B03002_005E", asian = "B03002_006E",
+        hawaiian = "B03002_007E", other = "B03002_008E",
+        multiracial = "B03002_009E", latinx = "B03002_012E", total = "B03002_001E",
+        medhhinc = "B19049_001E", agghhinc = "B19025_001E", hu = "B25001_001E")
 
-dat <- load_variables(YR, DAT)
+# dat <- load_variables(YR, DAT)
 
 ## get county level data
-cnty<- get_acs(geography = "county",
+cnty<- get_acs(geography = "block group",
                variables = VAR,
                state = ST,
                county = CNTY,
@@ -40,9 +40,12 @@ cnty<- get_acs(geography = "county",
                geometry = TRUE,
                keep_geo_vars = TRUE)
 
+chr <- cnty %>% 
+  filter(GEOID == '450259505021' | GEOID == '450259505022')
+
 ## get block group data 
-bg <- get_decennial(geography = "block",
-                 variables = 	'P1_001N',
+bg <- get_acs(geography = "block group",
+                 variables = 	'VAR',
                  state = ST,
                  county = CNTY,
                  year = YR,
