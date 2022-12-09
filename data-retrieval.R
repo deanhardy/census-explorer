a295e7c607b744faeaeef3331089482268dd2dac
“Dean <deanhardy@gmail.com>
2022-12-09 13:16:00 -0500

    updates

--- data-retrieval.R
+++ data-retrieval.R
@@ -19,9 +19,9 @@ alb <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-84 +x_0=0 +y_0=0 +el
 
 ## define census year, geography, & variables
 DAT <- 'acs5'
-YR <- 2013
-ST <- c('SC')
-CNTY <- c('Chesterfield')
+YR <- 2019
+ST <- c('GA')
+CNTY <- c('McIntosh')
 VAR = c(white = "B03002_003E", black = "B03002_004E",
         native_american = "B03002_005E", asian = "B03002_006E",
         hawaiian = "B03002_007E", other = "B03002_008E",
@@ -40,12 +40,9 @@ cnty<- get_acs(geography = "block group",
                geometry = TRUE,
                keep_geo_vars = TRUE)
 
-chr <- cnty %>% 
-  filter(GEOID == '450259505021' | GEOID == '450259505022')
-
 ## get block group data 
 bg <- get_acs(geography = "block group",
-                 variables = 	'VAR',
+                 variables = 	VAR,
                  state = ST,
                  county = CNTY,
                  year = YR,
@@ -60,14 +57,23 @@ bg2 <- bg %>%
   mutate(sqkm_bg = as.numeric(st_area(geometry)) / 1e6, mnhhinc = agghhinc/hu,
          propPOC = 1 - (white/total)) %>%
   dplyr::select(GEOID, ALAND, AWATER, sqkm_bg, total, white, black, native_american, asian, hawaiian,
-                other, multiracial, latinx, propPOC, medhhinc, agghhinc, hu, mnhhinc) %>%
-  filter(GEOID != 130519900000)
+                other, multiracial, latinx, propPOC, medhhinc, agghhinc, hu, mnhhinc)
 
 ## quick map of census data
 tm_shape(bg2) + 
   tm_polygons(col = 'propPOC')
 
+
+## get block group data 
+age10 <- get_decennial(geography = "block",
+                       state = ST,
+                       county = CNTY,
+                       variables = "P013001", 
+                       year = 2010,
+                       geometry = T)
+
 ## interactive map of census data
 tmap_mode(mode = c('view'))
-tm_shape(bg) + 
-  tm_polygons(col = 'P1_001N')
+tm_shape(age10) + 
+  tm_polygons(col = 'value')
+
